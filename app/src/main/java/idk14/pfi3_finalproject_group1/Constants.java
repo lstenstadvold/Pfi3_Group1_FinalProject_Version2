6040f54b1b853a1f4b8bece81186663b3ed38770
lstenstadvold <lstenstadvold@gmail.com>
2015-05-12 14:18:53 +0200

    Transfered stuff from old project.

---
+++ app/src/main/java/idk14/pfi3_finalproject_group1/Constants.java
@@ -0,0 +1,8 @@
+package idk14.pfi3_finalproject_group1;
+
+/**
+ * Created by larsy09 on 5/11/15.
+ */
+public class Constants {
+    static final String FIREBASE_URL = "https://blinding-torch-9917.firebaseio.com";
+}
---
+++ app/src/main/java/idk14/pfi3_finalproject_group1/Treasure.java
@@ -0,0 +1,24 @@
+package idk14.pfi3_finalproject_group1;
+
+/**
+ * Created by larsy09 on 5/11/15.
+ */
+public class Treasure {
+
+    public String treasureType;
+
+    public Treasure() {
+    }
+
+    public Treasure(String treasureType) {
+        this.treasureType = treasureType;
+    }
+
+    public String getTreasureType() {
+        return treasureType;
+    }
+
+    public void setTreasureType(String treasureType) {
+        this.treasureType = treasureType;
+    }
+}
