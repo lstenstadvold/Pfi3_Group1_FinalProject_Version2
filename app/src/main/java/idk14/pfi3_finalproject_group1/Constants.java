aa7cfa503886a538f2d0b105a4a1262a529e6d2b
Lars <K3LARRA@HOTMAIL.COM>
2015-05-13 10:38:07 +0200

    Liten ändring

--- app/src/main/java/idk14/pfi3_finalproject_group1/Constants.java
+++ app/src/main/java/idk14/pfi3_finalproject_group1/Constants.java
@@ -1,7 +1,7 @@
 package idk14.pfi3_finalproject_group1;
 
 /**
- * Created by lars09 on 5/11/15.....
+ * Created by lars09 on 5/11/15.....Och ännu mer
  */
 public class Constants {
     static final String FIREBASE_URL = "https://blinding-torch-9917.firebaseio.com";
