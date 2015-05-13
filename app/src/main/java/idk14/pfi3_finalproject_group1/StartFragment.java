d4fa1507467445c6e8df75e19cef29e8dfd79724
anna0314 <chocolatebeanbag@gmail.com>
2015-05-13 13:00:00 +0200

    test

--- app/src/main/java/idk14/pfi3_finalproject_group1/StartFragment.java
+++ app/src/main/java/idk14/pfi3_finalproject_group1/StartFragment.java
@@ -1,6 +1,8 @@
 package idk14.pfi3_finalproject_group1;
 
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -33,23 +35,51 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_start, container, false); //Fills the layout
         Button button = (Button) v.findViewById(R.id.button);
-
-
-
         button.setOnClickListener(this);
 
         return v;
     }
-//test
 
 
     @Override
     public void onClick(View view) {
-        if(view.getId()==R.id.button){
+        if (view.getId() == R.id.button) {
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id==R.id.action_settings){
+            return true;
+        }
+
+        if (id == R.id.Map) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.start_layout, new MapFragment());
+            ft.addToBackStack(null);
+            ft.commit();
+            return false;
+        }
+
+        if (id == R.id.About) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.start_layout, new HelpFragment());
+            ft.addToBackStack(null);
+            ft.commit();
+            return false;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
@@ -65,29 +95,8 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 }
 
 
-}
+
