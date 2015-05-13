fa4176d11159f894219c2cc3fe2261bddfccde96
lstenstadvold <lstenstadvold@gmail.com>
2015-05-13 12:04:40 +0200

    Integrated qr scanning, seems to work!

--- app/src/main/java/idk14/pfi3_finalproject_group1/StartFragment.java
+++ app/src/main/java/idk14/pfi3_finalproject_group1/StartFragment.java
@@ -19,7 +19,7 @@ import android.widget.Toast;
  */
 public class StartFragment extends Fragment implements View.OnClickListener {
 
-    private TextView formatTxt, contentTxt;
+   public TextView contentTxt;
 
 
     public StartFragment() {
@@ -33,6 +33,7 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_start, container, false); //Fills the layout
         Button button = (Button) v.findViewById(R.id.button);
+        contentTxt = (TextView) v.findViewById(R.id.scan_content);
 
 
 
@@ -56,38 +57,13 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         if (scanningResult != null) {
             String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
-            formatTxt.setText("FORMAT: " + scanFormat);
             contentTxt.setText("CONTENT: " + scanContent);
         } else {
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "No scan data received!", Toast.LENGTH_SHORT);
-            toast.show();
+           // Toast toast = Toast.makeText(getApplicationContext(),
+           //         "No scan data received!", Toast.LENGTH_SHORT);
+           // toast.show();
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-}
 
 
 }
