3b72525dea41ed316cfa1199a85e377abbf25337
anna0314 <chocolatebeanbag@gmail.com>
2015-05-19 13:18:54 +0200

    Cleaned

--- app/src/main/java/idk14/pfi3_finalproject_group1/StartFragment.java
+++ app/src/main/java/idk14/pfi3_finalproject_group1/StartFragment.java
@@ -48,12 +48,10 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         Button button = (Button) v.findViewById(R.id.button);
         //contentTxt = (TextView) v.findViewById(R.id.scan_content);
 
-
         button.setOnClickListener(this);
 
         return v;
     }
-//test
 
 
     @Override
@@ -82,9 +80,9 @@ public class StartFragment extends Fragment implements View.OnClickListener {
             }
 
         } else {
-            // Toast toast = Toast.makeText(getApplicationContext(),
-            //         "No scan data received!", Toast.LENGTH_SHORT);
-            // toast.show();
+/*            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();*/
         }
     }
 
