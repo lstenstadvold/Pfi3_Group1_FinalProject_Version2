a7039e982295b37fdba3cf779fff9fb38defb14d
lstenstadvold <lstenstadvold@gmail.com>
2015-05-20 12:42:33 +0200

    Created a dialogfragment that shows up when the smartphone is not connected to Firebase (NoConnectionFragment) and a dialog that shows up when the user scans the home spot without treasure (WrongScanFragment).

---
+++ app/src/main/java/idk14/pfi3_finalproject_group1/WrongScanFragment.java
@@ -0,0 +1,38 @@
+package idk14.pfi3_finalproject_group1;
+
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WrongScanFragment extends DialogFragment implements View.OnClickListener {
+
+
+    public WrongScanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_wrong_scan, container, false);
+
+        v.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        WrongScanFragment.this.dismiss();
+    }
+}
