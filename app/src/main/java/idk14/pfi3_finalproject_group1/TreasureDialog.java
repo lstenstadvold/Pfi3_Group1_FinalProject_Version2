84bce67e1e9fabb08905ef6d446443a3fa1404e7
lstenstadvold <lstenstadvold@gmail.com>
2015-06-02 12:35:23 +0200

    small change

--- app/src/main/java/idk14/pfi3_finalproject_group1/TreasureDialog.java
+++ app/src/main/java/idk14/pfi3_finalproject_group1/TreasureDialog.java
@@ -67,7 +67,7 @@ public class TreasureDialog extends DialogFragment implements View.OnClickListen
             treasureText.setText("Nothing here!");
             treasureImage.setImageDrawable(getResources().getDrawable(R.drawable.treasure_empty));
             desText.setVisibility(View.INVISIBLE);
-            scanButton.setVisibility(View.INVISIBLE);
+            scanButton.setVisibility(View.GONE);
 
 
 
